d9dd4a0aef0e09b32448a7012fdbd93e09461877
TheSmileyMan <jesusperalta1026@gmail.com>
2018-07-21 01:38:15 -0600

    Fixed a problem that I accidently left
    God dam Github these days.

--- main/java/com/ubalube/scifiaddon/init/ModItems.java
+++ main/java/com/ubalube/scifiaddon/init/ModItems.java
@@ -1,33 +1,13 @@
 package com.ubalube.scifiaddon.init;
 
-import java.util.ArrayList;
-import java.util.List;
-
-<<<<<<< HEAD
-import com.ubalube.scifiaddon.items.*;
-
-=======
 import com.ubalube.scifiaddon.items.GuideBook;
-import com.ubalube.scifiaddon.items.ItemBase;
 import com.ubalube.scifiaddon.items.Redstonewand;
 import com.ubalube.scifiaddon.items.RifleBase;
->>>>>>> 843fb6976ab0183af5be871b4cdc078b08d65460
 import com.ubalube.scifiaddon.util.ItemHelper;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.init.SoundEvents;
-import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
-import net.minecraft.item.Item.ToolMaterial;
-import net.minecraft.item.ItemArmor.ArmorMaterial;
-import net.minecraft.item.ItemStack;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.world.World;
 import net.minecraftforge.client.event.ModelRegistryEvent;
-import net.minecraftforge.common.util.EnumHelper;
 import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
@@ -58,13 +38,7 @@ public class ModItems
 		//ItemHelper.registerItemModel(); <-- That is the base, First goes the Name Given, then the modid, I think you can just do Reference.MOD_IDm and the end is the name
 		//The Item Renderer is me testing something, but it still doesn't work.
 		ItemHelper.registerItemModel(RIFLE, "sfa", "rifle");
-<<<<<<< HEAD
 		ItemHelper.registerItemModel(REDSTONEWAND, "sfa", "redstonewand");
-=======
-		
-		ItemHelper.registerItemModel(REDSTONEWAND, "sfa", "redstonewand");
-		
->>>>>>> 843fb6976ab0183af5be871b4cdc078b08d65460
 		ItemHelper.registerItemModel(GUIDEBOOK, "sfa", "guidebook");
 	}
 
